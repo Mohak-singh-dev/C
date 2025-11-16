9eb4eb308cb83e77dd20c9593131a8438dca0a8e
Mohak Singh <mohak_singh@outlook.com>
2025-11-17 02:42:56 +0530

    Delete 02_Operators directory

--- 02_Operators/comparison_ternary.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-void main(){
-    int a,b;
-    printf("Enter two numbers : ");
-    scanf("%d %d",&a,&b);
-    printf("Highest : %d\n",(a>b)? a:b);
-    printf("Lowest : %d\n",(a<b)? a:b);
-}
