6f3c1540eeb18dfa1ff6b60913cc3f418db9ad9b
Mohak-singh-dev <singhmohak360@gmail.com>
2025-11-17 01:18:34 +0530

    Size of different data types

--- Basics/size.c
+++ Basics/size.c
@@ -1,11 +1,21 @@
 #include<stdio.h>
 void main(){
     int integer;
+    long L_int;
+    long long LL_int;
+    signed int S_int;
+    unsigned int U_int;
     float flt;
     double dble;
+    long double L_dble;
     char character;
-    printf("%d\n",sizeof(integer));
-    printf("%d\n",sizeof(flt));
-    printf("%d\n",sizeof(dble));
-    printf("%d\n",sizeof(character));
+    printf("Integer : %d\n",sizeof(integer));
+    printf("Long Integer : %d\n",sizeof(L_int));
+    printf("Long Long Integer : %d\n",sizeof(LL_int));
+    printf("Signed Integer : %d\n",sizeof(S_int));
+    printf("Unsigned Integer : %d\n",sizeof(U_int));
+    printf("Float : %d\n",sizeof(flt));
+    printf("Double : %d\n",sizeof(dble));
+    printf("Long Double : %d\n",sizeof(L_dble));
+    printf("Character : %d\n",sizeof(character));
 }
