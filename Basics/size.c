6a43e7087b4c5a5addc715fa389c573e90f69ee1
Mohak-singh-dev <singhmohak360@gmail.com>
2025-11-17 01:08:28 +0530

    size of different data types

---
+++ Basics/size.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(){
+    int integer;
+    float flt;
+    double dble;
+    char character;
+    printf("%d\n",sizeof(integer));
+    printf("%d\n",sizeof(flt));
+    printf("%d\n",sizeof(dble));
+    printf("%d\n",sizeof(character));
+}
