7436f6a47e940a4c590326ffb010733a1b8e8b8a
Mohak-singh-dev <singhmohak360@gmail.com>
2025-11-17 02:35:32 +0530

    Update

---
+++ 02_Operator/comparison_ternary.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main(){
+    int a,b;
+    printf("Enter two numbers : ");
+    scanf("%d %d",&a,&b);
+    printf("Highest : %d\n",(a>b)? a:b);
+    printf("Lowest : %d\n",(a<b)? a:b);
+}
