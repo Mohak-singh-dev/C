e0acd597f2ba2eaa8ce3d6543ffc821b48b6b33f
Mohak-singh-dev <singhmohak360@gmail.com>
2025-11-17 00:31:07 +0530

    first commit

---
+++ Basics/Variables.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(){
+    int a,b;
+    printf("Enter two numbers: ");
+    scanf("%d %d",&a,&b);
+    printf("Sum : %d\n",a+b);
+    printf("Difference : %d\n",a-b);
+    printf("Product : %d\n",a*b);
+    printf("Division : %d\n",a/b);
+    printf("Remainder : %d\n",a%b);
+}
